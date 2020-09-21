2ae3aae461843d402de69819e8fe28b6fd6b8551
Kim Torres <kimberley.5118@gmail.com>
2020-09-21 11:17:38 -0400

    beginning to work on js functions to iterate through the quiz

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var start = document.getElementById("btn btn-info");
+var startEl = document.querySelector("#start");
+console.log(startEl)
+var timerEl = document.querySelector("timer");
+var clearEl = document.querySelector("#clear");
+var questionEl = document.querySelector(".question");
+var answerEl = document.querySelector("#answer");
+var submitEl = document.querySelector("#submit");
+var homeEl = document.querySelector(".cq");
+var liEl = document.querySelectorAll("li");
+console.log(liEl);
 
 // create a constant array variable that holds question objects with details of questions, choices and answer
 const quiz = [
@@ -10,7 +19,7 @@ const quiz = [
         c: "numbers",
         d: "alerts"
              },
-        answer: "d"
+        answer: "alerts"
    },
    {
        question:"The condition in an if/else statement is enclosed within?",
@@ -20,17 +29,17 @@ const quiz = [
          c:"parentheses",
          d:"square brakets",
           },
-        answer: "b",
+        answer: "c",
     },
     {
         question:"Arrays in Javascript can be used to store",
         choices:{
             a: "numbers and strings",
             b: "other arrays",
-            c: "booleans",
+            c: "objects",
             d: "all of the above",
         },
-        answer: "d",
+        answer: "all of the above",
     },
     {
         question: "String values are enclosed within _____ when being assigned to variables",
@@ -38,23 +47,64 @@ const quiz = [
             a: "commas",
             b: "curly brackets",
             c: "quotes",
-            d: "parenthesis",
+            d: "parentheses",
         },
-        answer:"c",
+        answer:"quotes",
     },
      ];
    
+// I need to put in a form when they are done the quiz to submit their highs score.I am not sure where that form goes, in the html? the form will have a submit button 
+// so I will give that an event listener 
+
 var questionIndex = 0;
-         
+ 
+//when the quiz is started I have to remove the display none css setting so that the questions can be seen
 function startQuiz(){
-    const output = [];
-    
-    // question.forEach(
-    // )
+console.log("start");  
+homeEl.textContent = "";
+questionEl.textContent = quiz[questionIndex].question
+console.log(Object.values(quiz[questionIndex].choices))
+var answers = Object.values(quiz[questionIndex].choices);
+
+for(i =0; i<answers.length; i++){
+    console.log(answers[i]);
+    console.log(liEl[i]);
+    liEl[i].textContent = answers[i];
+
+    liEl[i].addEventListener("click",rightOrWrong); 
 
 }
 
+};
 
-start.addEventListener("click", function(){
-    startQuiz();
-});
+function rightOrWrong(event){
+    console.log(event.target)
+
+    console.log (event.target.textContent)
+
+    if(event.target.textContent === quiz[questionIndex].answer){
+        console.log("correct");
+       
+    }
+    else{
+
+    }
+       
+    };
+
+//event listener that will start the code 
+startEl.addEventListener("click", startQuiz);
+
+// //do I need an event listener so that when clicked it will go to the next question?
+// //also if they select the right answer it should display "correct" underneath.
+// answerEl.addEventListener("click", function(){
+
+// });
+
+// //how can I create the "wrong!" notification
+
+// // I also need to figure out the timer, how to get it to decrement correctly
+
+// submitEl.addEventListener("click", function(){
+
+// });
